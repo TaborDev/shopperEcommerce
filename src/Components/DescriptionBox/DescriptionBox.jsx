84154b374a389b6007f1bfb76c8a49dc46ea5ef5
Tawer Aradom <taweraradom@gmial.com>
2023-12-13 21:14:26 +0300

    final commit

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+        <div className="descriptionbox-navigator">
+            <div className="descriptionbox-nav-box">Description</div>
+            <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+        </div>
+        <div className="descriptionbox-description">
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum, odit? Error corrupti nemo culpa hic tenetur voluptas labore, deleniti architecto esse quidem possimus! Dolorem, dicta reprehenderit minima ipsa ipsam illum modi hic aliquid possimus laborum itaque laboriosam tempore ea rerum debitis veritatis accusantium corporis minus voluptates eos vero suscipit velit vel molestias? Voluptatem repellat saepe ea eveniet inventore! Officia, explicabo?</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores numquam nihil accusamus obcaecati rem molestias tempore quos reprehenderit nulla, sapiente ullam! Quos deserunt odio quas et amet. Cumque, velit? Iusto qui ullam repudiandae fuga ipsum beatae labore vitae minima! Illo.</p>
+        </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
